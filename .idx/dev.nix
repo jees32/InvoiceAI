71ad4553bf00972fbdb6384370380451abbfea8f
EL-shaddai. Storage <elshaddai.cloudstack@gmail.com>
2025-07-16 04:03:47 -0400

    Fix RSC payload fetch error

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,7 +9,9 @@
     pkgs.zulu
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    NEXT_PUBLIC_APP_URL = "${WEB_CONTAINER_URL}";
+  };
   # This adds a file watcher to startup the firebase emulators. The emulators will only start if
   # a firebase.json file is written into the user's directory
   services.firebase.emulators = {
